c88e1b0e97261ab42c2f583d2ad3e4e0e02e1d8e
Ariel201711 <arielk216@gmail.com>
2022-08-24 20:51:48 +0300

    Rendering a Mail List With Unread Indidcation

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -1,3 +1,13 @@
-export function MailSort() {
-    return <h1>Hi From Mail Preview</h1>
+export function MailPreview({ mail }) {
+
+    function getReadClass(mail) {
+        return !mail.isRead? 'unread': ''
+    }
+
+    return <div className="flex align-center">
+        <div className={`mail-subject ${getReadClass(mail)}`}>{mail.subject}</div>
+        <div className={"mail-body"}>{mail.body}</div>
+        <div className={`mail-sentAt ${getReadClass(mail)}`}>{mail.sentAt}</div>
+        <div className={`mail-to ${getReadClass(mail)}`}>{mail.to}</div>
+    </div>
 }
