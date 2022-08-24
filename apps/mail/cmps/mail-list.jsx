26a33ea2c4e6d7ed0b7c206892814901d4d08d3c
Ariel201711 <arielk216@gmail.com>
2022-08-25 00:13:14 +0300

    added remove mail feature

--- apps/mail/cmps/mail-list.jsx
+++ apps/mail/cmps/mail-list.jsx
@@ -1,10 +1,10 @@
 import { MailPreview } from "../cmps/mail-preview.jsx";
 
-export function MailList({ mails }) {
+export function MailList({ mails, onRemoveMail, onAddMail }) {
     return <section className="mail-list">
         <ul>
             {mails.map(mail => <li key={mail.id} className="mail-preview clean-list flex">
-                <MailPreview mail={mail} />
+                <MailPreview mail={mail} onRemoveMail={onRemoveMail}/>
             </li>)}
         </ul>
     </section>
--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -1,13 +1,14 @@
-export function MailPreview({ mail }) {
+export function MailPreview({ mail, onRemoveMail}) {
 
     function getReadClass(mail) {
         return !mail.isRead? 'unread': ''
     }
 
-    return <div className="flex align-center">
+    return <div className="mail-preview-div flex align-center">
         <div className={`mail-subject ${getReadClass(mail)}`}>{mail.subject}</div>
         <div className={"mail-body"}>{mail.body}</div>
         <div className={`mail-sentAt ${getReadClass(mail)}`}>{mail.sentAt}</div>
         <div className={`mail-to ${getReadClass(mail)}`}>{mail.to}</div>
+        <button className="remove-mail" onClick={() => onRemoveMail(mail.id)}><img src="../../../assets/icons/trash.png"/></button>
     </div>
 }
