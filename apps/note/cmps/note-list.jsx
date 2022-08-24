29eb9a9f40f94ef7a136a97f2260234a40198f14
Ron Shaked <ronshaked1266@gmail.com>
2022-08-24 15:04:31 +0300

    storge

--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -1,17 +1,18 @@
 import { NotePreview } from './note-preview.jsx'
 
-export function NoteList() {
+export function NoteList({ notes, onRemoveNote }) {
     return <section className="note-list">
         <ul>
             {
-                <li className="note-preview">
-                    <NotePreview />
+                notes.map(note =>
+                <li className="note-preview" key={note.id}>
+                    <NotePreview note={note}/>
                     <button>✖</button>
                     <button>🎬</button>
                     <button>📌</button>
-                    <button><img src="assets/icons/paint-board.png"/></button>
-                    <button><img src="assets/icons/trash.png"/></button>
-                </li>
+                    <button><img src="assets/icons/paint-board.png" /></button>
+                    <button onClick={()=>onRemoveNote(note.id)}><img src="assets/icons/trash.png" /></button>
+                </li>)
             }
         </ul>
     </section>
--- apps/note/views/note-index.jsx
+++ apps/note/views/note-index.jsx
@@ -1,27 +1,53 @@
 import { NoteFilter } from './../cmps/note-filter.jsx'
 import { NoteList } from './../cmps/note-list.jsx'
 import { noteService } from '../../../services/note-service.js'
+import { showErrorMsg, showSuccessMsg } from '../../../services/event-bus.service.js'
 export class NoteIndex extends React.Component {
     state = {
         notes: [],
-        filterBy: null, 
+        filterBy: null,
+        isBounce: false
     }
 
-    // componentDidMount() {
-    //     this.loadNotes()
-    // }
+    componentDidMount() {
+        this.loadNotes()
+    }
 
+    loadNotes = () => {
+        noteService.query()
+            .then((notes) => this.setState({ notes }))
+    }
     // loadNotes = () => {
     //     noteService.query(this.state.filterBy)
     //         .then((notes) => this.setState({ notes }))
     // }
-    
+
+    onRemoveNote = (noteId) => {
+        noteService.remove(noteId)
+            .then(() => {
+                console.log('Removed!')
+                const notes = this.state.notes.filter(note => note.id !== noteId)
+                this.setState({ notes, isBounce: true })
+                showSuccessMsg('note removed')
+                setTimeout(()=>{
+                    this.setState({ isBounce: false })
+                }, 500)
+
+            })
+            .catch(err => {
+                console.log('Problem!!', err)
+                showErrorMsg('Cannot remove note')
+            })
+    }
+
     render() {
+        const { notes } = this.state
+        const { onRemoveNote } = this
         return (
             <section className="note-index main-layout">
                 <div>note app</div>
                 <NoteFilter />
-                <NoteList />
+                <NoteList notes={notes} onRemoveNote={onRemoveNote} />
             </section>
         )
     }
