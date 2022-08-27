d6cbc35846041440bbd358fc2f221bfdb53e6112
Ron Shaked <ronshaked1266@gmail.com>
2022-08-28 01:42:10 +0300

    home class

--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,7 @@ export function Home() {
         {/* <h1>Welcome to home page!</h1> */}
 
         <div className="home-links">
-        <NavLink className="book-link" exact to="/book"><i class="fa-solid fa-book"></i></NavLink>
+        <NavLink className="book-link" exact to="/book"><i className="fa-solid fa-book"></i></NavLink>
         <NavLink className="mail-link" exact to="/mail"><img src="assets/icons/mail.webp" /></NavLink>
         <NavLink className="note-link" exact to="/note"><img src="assets/icons/note.png" /></NavLink>
         </div>
