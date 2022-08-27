a50c0ff4e8ef90270d9ecf17e50b2937337e47ed
Ron Shaked <ronshaked1266@gmail.com>
2022-08-28 01:12:57 +0300

    home

--- views/home.jsx
+++ views/home.jsx
@@ -1,7 +1,13 @@
-
+const { NavLink} = ReactRouterDOM
 export function Home() {
 
-    return <section className="home main-layout">
-        <h1>Welcome to home page!</h1>
+    return <section className="home">
+        {/* <h1>Welcome to home page!</h1> */}
+
+        <div className="home-links">
+        <NavLink className="book-link" exact to="/book"><i class="fa-solid fa-book"></i></NavLink>
+        <NavLink className="mail-link" exact to="/mail"><img src="assets/icons/mail.webp" /></NavLink>
+        <NavLink className="note-link" exact to="/note"><img src="assets/icons/note.png" /></NavLink>
+        </div>
     </section>
 }
